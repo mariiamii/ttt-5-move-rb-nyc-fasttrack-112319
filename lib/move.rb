3e6fc6cb79726f2192354bf2a3718b752b1cdd94
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-11-25 22:58:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,10 +11,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(array, index, value)
-  
-end
-
 def move(board, index, current_player = "X")
   board[index] = current_player
 end
